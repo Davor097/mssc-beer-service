a8d06df11c688d638f9328635cf97120ce06afbc
Davor <davor097@gmail.com>
2023-11-13 17:16:48 +0100

    Added beer repository

---
+++ src/main/java/com/davor/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.davor.msscbeerservice.repositories;
+
+import com.davor.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
