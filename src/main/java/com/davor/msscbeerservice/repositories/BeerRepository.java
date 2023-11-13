d4bf83496d11de4ac4b1fa010ff6fdfce51b4395
Davor <davor097@gmail.com>
2023-11-13 17:27:27 +0100

    Added bootstrap

--- src/main/java/com/davor/msscbeerservice/repositories/BeerRepository.java
+++ src/main/java/com/davor/msscbeerservice/repositories/BeerRepository.java
@@ -1,9 +1,10 @@
 package com.davor.msscbeerservice.repositories;
 
 import com.davor.msscbeerservice.domain.Beer;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.UUID;
 
-public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+public interface BeerRepository extends JpaRepository<Beer, UUID> {
 }
